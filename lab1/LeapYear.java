134c59b52a566834ff1624d4af0d4d4665099aea
xiexingsi <noodlesfork6@gmail.com>
2025-01-15 23:33:28 +0800

    complete leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -31,3 +31,20 @@ public class LeapYear {
     }
 }
 
+
+/**
+ *
+ * @param year
+ * @return boolean, whether the given year is a leap year.
+ */
+public static boolean isLeapYear(int year){
+    if (year % 400 == 0){
+        return true;
+    } else if ((year % 4 == 0) & (year % 100 != 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
