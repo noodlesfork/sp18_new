343515d331b3d50f4970ef808fb3d41ba0faa03d
xiexingsi <noodlesfork6@gmail.com>
2025-01-23 03:24:40 +0800

    proj1gold fourth try

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -27,29 +27,29 @@ public class TestArrayDequeGold {
         }
     }
 
-    @Test
-    public void TestAddFirstRemoveLast(){
-
-        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
-
-        int n = 5;
-        String output = "";
-
-        for (int i = 0; i < 15; i ++) {
-            int A = StdRandom.uniform(n);
-            sad1.addFirst(A);
-            ads1.addFirst(A);
-            output += String.format("addFirst(%d)", A);
-            output += "\n";
-        }
-
-        for (int i = 0; i < 15; i ++) {
-            output += "removeLast";
-            assertEquals(output, sad1.removeLast(), ads1.removeLast());
-            output += "\n";
-        }
-    }
+//    @Test
+//    public void TestAddFirstRemoveLast(){
+//
+//        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+//        ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
+//
+//        int n = 5;
+//        String output = "";
+//
+//        for (int i = 0; i < 15; i ++) {
+//            int A = StdRandom.uniform(n);
+//            sad1.addFirst(A);
+//            ads1.addFirst(A);
+//            output += String.format("addFirst(%d)", A);
+//            output += "\n";
+//        }
+//
+//        for (int i = 0; i < 15; i ++) {
+//            output += "removeLast";
+//            assertEquals(output, sad1.removeLast(), ads1.removeLast());
+//            output += "\n";
+//        }
+//    }
 
 //    @Test
 //    public void TestAddFirstRemoveFirst(){
